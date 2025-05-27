f40f15b80cf930d133ead32f950e961a5e8d0656
Julián Guzmán <jguzmanlongoria2@gmail.com>
2025-05-27 16:23:35 -0600

    Adds Paging 3 support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,28 +54,42 @@ android {
 }
 
 dependencies {
-
     implementation(libs.timber)
+
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.logging.interceptor)
+
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.paging)
+
+    implementation(libs.androidx.paging.runtime.ktx)
+    implementation(libs.androidx.paging.compose)
+
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.hilt.android)
     ksp(libs.dagger.hilt.compiler)
+
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
+
     implementation(libs.coil.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+
     implementation(libs.androidx.material3)
+
+    implementation(libs.androidx.paging.common.ktx)
     implementation(libs.kotlinx.coroutines.test)
     implementation(libs.junit.jupiter.api)
     implementation(libs.junit.jupiter.params)
@@ -84,7 +98,9 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.assertk)
     testImplementation(libs.mockk)
+
     testRuntimeOnly(libs.junit.jupiter.engine)
+
     androidTestImplementation(libs.androidx.runner)
     androidTestImplementation(libs.junit5.test.core)
     androidTestImplementation(libs.assertk)
@@ -92,6 +108,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
