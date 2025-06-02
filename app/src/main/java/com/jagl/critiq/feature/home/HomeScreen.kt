a5ea58e89d82f383ef4c2d2d6108a93706ee761c
Julián Guzmán <jguzmanlongoria2@gmail.com>
2025-06-01 18:17:51 -0600

    Updated HomeScreen

--- app/src/main/java/com/jagl/critiq/feature/home/HomeScreen.kt
+++ app/src/main/java/com/jagl/critiq/feature/home/HomeScreen.kt
@@ -1,16 +1,14 @@
 package com.jagl.critiq.feature.home
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,29 +23,35 @@ import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
 import com.jagl.critiq.core.model.Media
+import com.jagl.critiq.core.model.UiMessage
+import com.jagl.critiq.core.test.media
+import com.jagl.critiq.core.ui.components.Loading
+import com.jagl.critiq.core.ui.components.NotAvable
 import com.jagl.critiq.core.ui.components.RatingBar
+import com.jagl.critiq.core.ui.extensions.fullScreen
 import kotlinx.coroutines.flow.flow
 
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
-    viewModel: HomeViewModel = hiltViewModel()
+    viewModel: HomeViewModel = hiltViewModel(),
+    onNavigateToDetail: (Long) -> Unit
 ) {
     val items = viewModel.items.collectAsLazyPagingItems()
-    HomeContent(modifier, items)
+    HomeContent(modifier, items, onNavigateToDetail)
 }
 
 @Composable
 private fun HomeContent(
-    modifier: Modifier = Modifier.fillMaxSize(),
-    items: LazyPagingItems<Media>
+    modifier: Modifier = Modifier.fullScreen(),
+    items: LazyPagingItems<Media>,
+    onClick: (Long) -> Unit
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top,
-        modifier = Modifier.then(modifier)
+        modifier = modifier
     ) {
-
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
         ) {
@@ -57,8 +61,8 @@ private fun HomeContent(
                 Card(modifier = Modifier.padding(8.dp)) {
                     Column(
                         modifier = Modifier
-                            .fillMaxSize()
-                            .padding(4.dp),
+                            .fullScreen(padding = 4.dp)
+                            .clickable { onClick(item.id) },
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         AsyncImage(
@@ -83,20 +87,15 @@ private fun HomeContent(
                 when {
                     loadState.append is LoadState.Loading -> {
                         item {
-                            Box(
-                                modifier = Modifier.fillMaxWidth(),
-                                contentAlignment = Alignment.Center
-                            ) {
-                                CircularProgressIndicator()
-                            }
+                            Loading(modifier = modifier)
                         }
                     }
 
                     loadState.append is LoadState.Error -> {
                         item {
-                            Text(
-                                text = "Error loading more items",
-                                modifier = Modifier.padding(16.dp)
+                            NotAvable(
+                                modifier = modifier,
+                                text = UiMessage.Text("Error loading more items")
                             )
                         }
 
@@ -110,44 +109,13 @@ private fun HomeContent(
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
+    val medias: List<Media> = listOf(
+        media(), media(),
+        media(), media()
+    )
+    val items = flow { emit(PagingData.from(medias)) }.collectAsLazyPagingItems()
     HomeContent(
-        items = flow<PagingData<Media>> {
-            emit(
-                PagingData.from(
-                    listOf(
-                        Media(
-                            1,
-                            "Movie 1",
-                            "https://example.com/poster1.jpg",
-                            "https://example.com/backdrop1.jpg",
-                            "movie",
-                            8.5,
-                            "2023-01-01",
-                            "Description 1"
-                        ),
-                        Media(
-                            2,
-                            "Movie 2",
-                            "https://example.com/poster2.jpg",
-                            "https://example.com/backdrop2.jpg",
-                            "movie",
-                            7.0,
-                            "2023-02-01",
-                            "Description 2"
-                        ),
-                        Media(
-                            3,
-                            "Movie 3",
-                            "https://example.com/poster3.jpg",
-                            "https://example.com/backdrop3.jpg",
-                            "movie",
-                            9.0,
-                            "2023-03-01",
-                            "Description 3"
-                        )
-                    )
-                )
-            )
-        }.collectAsLazyPagingItems(),
+        items = items,
+        onClick = {}
     )
 }
