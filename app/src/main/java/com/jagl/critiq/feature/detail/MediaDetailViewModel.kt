accf90a308a24bc8f31776ed4789d883409dc471
Julián Guzmán <jguzmanlongoria2@gmail.com>
2025-06-03 20:41:48 -0600

    Added logic to get media

--- app/src/main/java/com/jagl/critiq/feature/detail/MediaDetailViewModel.kt
+++ app/src/main/java/com/jagl/critiq/feature/detail/MediaDetailViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jagl.critiq.R
+import com.jagl.critiq.core.common.dispatcherProvider.DispatcherProvider
+import com.jagl.critiq.core.local.source.LocalPaginationMediaDataSource
 import com.jagl.critiq.core.model.UiMessage
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
@@ -16,7 +17,9 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MediaDetailViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle
+    savedStateHandle: SavedStateHandle,
+    private val localPaginationMediaDataSource: LocalPaginationMediaDataSource,
+    private val dispatcherProvider: DispatcherProvider
 ) : ViewModel() {
 
     private val mediaId: Long = savedStateHandle.get<Long>(MEDIA_ID) ?: 0L
@@ -31,13 +34,21 @@ class MediaDetailViewModel @Inject constructor(
                 initialValue = MediaDetailUiState()
             )
 
-    private fun init() = viewModelScope.launch {
+    private fun init() = viewModelScope.launch(dispatcherProvider.default) {
         println("MediaDetailViewModel initialized with mediaId: $mediaId")
-        delay(5000L)
-        _uiState.value = MediaDetailUiState(
-            isLoading = false,
-            errorMessage = UiMessage.Resource(R.string.generic_error)
-        )
+        val media = localPaginationMediaDataSource.getById(mediaId)
+        if (media != null) {
+            _uiState.value = MediaDetailUiState(
+                isLoading = false,
+                media = media
+            )
+        } else {
+            _uiState.value = MediaDetailUiState(
+                isLoading = false,
+                errorMessage = UiMessage.Resource(R.string.generic_error)
+            )
+        }
+
     }
 
     companion object {
